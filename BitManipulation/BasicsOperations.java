2d39fd000a376630bcd88782025510086161eabd
shivprasadnikam <shivvprasad.nikam@gmail.com>
2024-01-24 23:03:53 +0530

    Bit Manipulation

---
+++ BitManipulation/BasicsOperations.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+public class BasicsOperations {
+    public static void main(String[] args) {
+
+        System.out.println("Binary ADD 1 & 0 Output : " + (1 & 0));
+        System.out.println("Binary OR 1 | 0  Output : " + (1 | 0));
+        System.out.println("Binary XOR 1 ^ 0 Output : " + (1 ^ 0));
+        System.out.println("Binary NOT of 1 : " + (~1));
+        System.out.println("2<<3 Left Shift : " + (2 << 3));
+        System.out.println("2>>3 Right Shift : " + (2 >> 3));
+
+    }
+
+}
